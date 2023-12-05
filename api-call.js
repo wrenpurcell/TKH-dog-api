532386eefd0aee9c13d4bb8ec77fbf9a07da09b4
Wren Purcell <purcell856@gmail.com>
2023-12-04 21:00:52 -0500

    step 1

--- api-call.js
+++ api-call.js
@@ -3,14 +3,8 @@
 import axios from "axios"
 
 export default async function fetchData(){
-  try {
+
       const response = await axios.get("https://dog.ceo/api/breeds/image/random/3");
-      if(response.status !== 200){
-        throw new Error("some kind of error :(")
-      }
-      return response.data
-  }
-  catch(error){
-    console.error("Error: " + error)
-  }
+      return response
+ 
 }
--- main.js
+++ main.js
@@ -1,12 +1,5 @@
 import "./style.css"
 import fetchData  from "./api-call.js";
-import { displayDogPics } from "./display-data.js";
 
 const data = await fetchData()
-
-const arrayOfDogPics = data.message
-console.log(arrayOfDogPics)
-
-//pass data array to displayDogPics
-displayDogPics(arrayOfDogPics)
-
+console.log(data)
