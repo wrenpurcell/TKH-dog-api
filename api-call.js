c9abb6d8039fbfad9b91ce5a6461d4452922cd21
Wren Purcell <purcell856@gmail.com>
2023-12-04 21:44:41 -0500

    added comments

--- api-call.js
+++ api-call.js
@@ -1,10 +1,15 @@
 // https://dog.ceo/api/breeds/image/random/3
 
+//make sure to install axios via the terminal 'npm install -save axios'
 import axios from "axios"
 
+//create an async function and export it 
 export default async function fetchData(){
 
+    //await the response from making the GET request to the url
       const response = await axios.get("https://dog.ceo/api/breeds/image/random/3");
+
+      //return the response so we can access it outside of the function
       return response
  
 }
--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 import "./style.css"
+//import the fetchData function 
 import fetchData  from "./api-call.js";
 
+//call fetchData()
+//make sure to use await, since we are still waiting for a promise to resolve when we call fetchData
 const data = await fetchData()
+
+//console.log data to see the object we're getting back from the API call
 console.log(data)
